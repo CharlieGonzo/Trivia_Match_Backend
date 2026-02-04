ea26ed2fb18d860f8e50d467329e1f86d8a72f0f
charliegonzo <gonzc3@farmingdale.edu>
2026-02-03 22:21:22 -0500

    working sockets

---
+++ src/main/java/org/example/trivia_server_spring/SocketController.java
@@ -0,0 +1,25 @@
+package org.example.trivia_server_spring;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+@Controller
+public class SocketController {
+
+    private final SimpMessagingTemplate template;
+
+    // dependency injection
+    public SocketController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    //test unique rooms
+    @MessageMapping("{id}")
+    public void test(@DestinationVariable String id){
+        System.out.println("this method worked");
+        template.convertAndSend("/topic/" + id,"hello");
+    }
+}
