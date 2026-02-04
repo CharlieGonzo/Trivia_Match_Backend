8196050958f48759c09b5bb568d373da6b72139e
charliegonzo <gonzc3@farmingdale.edu>
2026-02-03 20:02:37 -0500

    first commit

---
+++ src/main/java/org/example/trivia_server_spring/TriviaServerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.trivia_server_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriviaServerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/trivia_server_spring/TriviaServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trivia_server_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaServerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
